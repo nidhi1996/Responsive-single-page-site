29d46dcc64ad23a73c5e4b7116f8a6d0175df483
Nidhi Soni <nidhisoni1796@gmail.com>
2022-04-07 16:35:13 +0530

    Final Commit
    Responsive Web Page

--- js/main.js
+++ js/main.js
@@ -37,9 +37,34 @@ function showSlides(n) {
 
 
 //contact form
-document.addEventListener("DOMContentLoaded", function() {
-    fields.name = document.getElementById('name');
-    fields.email = document.getElementById('email');
-    fields.phone = document.getElementById('phone');
-    fields.message = document.getElementById('message');
-   })
+function validate(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    error_message.style.padding = "10px";
+    var text;
+    if(name.length < 2){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number of 10 digits";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <= 50){
+      text = "Please Enter Message More Than 50 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
