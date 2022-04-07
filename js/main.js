ca2157ceb82fdae3fc6475b60c76c174734f86c8
Nidhi Soni <nidhisoni1796@gmail.com>
2022-04-07 15:17:04 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//header
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+// slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+//contact form
+document.addEventListener("DOMContentLoaded", function() {
+    fields.name = document.getElementById('name');
+    fields.email = document.getElementById('email');
+    fields.phone = document.getElementById('phone');
+    fields.message = document.getElementById('message');
+   })
